85a3c40531be1887a503017960a5dff26ad28b18
joshua <joshuaslui0203@gmail.com>
2021-10-18 18:10:26 +0200

    Add more info

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
 <?php include 'Templates/head.html' ;?>
+
+<body>
+<?php include 'Templates/header.html' ;?>
+
+<img src="assets/images/Modraxis.png" alt="" style="background-color: var(--color-main)" width="100%">
+
+<div class="background">
+    <div class="container text-center">
+        <p class="fs-1 bold">Quality plugins, at no cost.</span></p>
+        <p>Varumox strives to provide Minecraft plugin authors and users with an <b class="explicified">easy-to-use UI</b> offering <b class="explicified">completely free plugins</b>. Additionally, Varumox offers various <b class="explicified">authentication-free API routes</b> to get plugin information or files from. Varumox is <b class="explicified">completely open source</b> <a href="https://github.com/PsychOps/Varumox">on GitHub</a>.
+    </div>
+</div>
+
+</body>
+
+</html>
