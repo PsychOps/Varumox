b7df806dbd390cbbd272cfd9a73b7f4a1afeef41
Hilex23 <89878821+Hilex23@users.noreply.github.com>
2021-10-24 22:24:10 +0200

    Fix because things disappeared

--- index.php
+++ index.php
@@ -8,6 +8,35 @@
 
 <img src="assets/images/Varumox.png" alt="" style="background-color: var(--color-main)" width="100%">
 
+<div class="background" id="about">
+    <div class="container text-center" id="description">
+        <p class="bold fs-1 explicified">Quality plugins, at no cost.</span></p>
+        <p>Varumox strives to provide Minecraft plugin authors and users with an <a class="explicified bold" href="#UI">easy-to-use UI</a> offering <a class="explicified bold" href="#Free">completely free plugins</a>. Additionally, Varumox offers various <a class="explicified bold" href="#API">authentication-free API routes</a> to get plugin information or files from. Varumox is <b class="explicified">completely open source</b> <a href="https://github.com/PsychOps/Varumox">on GitHub</a>.
+    </div>
+
+    <div class="container topspace-50 text-start" id="UI">
+    <!-- <i class="fas fa-balance-scale fa-fw me-3"></i><span>Legal</span> -->
+        <a href=#UI>
+        <i class="fas fa-link explicified"></i>
+        <span class="bold fs-2 explicified">easy-to-use UI</span>
+        </a>
+        <p class="textwidth text-start">Varumox has a convenient and simple design, but pulls the most out of it by offering users a wide variety of features related to both searching for plugins, as well as uploading plugins and updating plugins on our website.</p>
+    </div>
+
+    <div class="container topspace-50 text-end" id="Free">
+            <a href=#Free>
+                <span class="bold fs-2 explicified">Completely free plugins</span>
+                <i class="fas fa-link explicified"></i>
+            </a>
+            <p class="text-end" style="margin-left: 50%">Varumox is free forever. This includes not only the project itself, but also the plugins listed on it. We do not offer any way to redirect to paid plugin websites or to make them paid on Varumox.</p>
+    </div>
+
+    <div class="container topspace-50 text-start" id="API">
+        <a href=#API>
+            <i class="fas fa-link explicified"></i>
+            <span class="bold fs-2 explicified">Authorization-free API</span>
+        </a>
+        <p class="textwidth text-start">Varumox offers a freely usable API to fetch data from in JSON format. Offering a variety of different and detailed, endpoints, it is designed to make getting data as easy as possible without the necessity of using an API token. The API can grab data about both authors and plugins, including plugin files.</p>
 <div class="background">
     <div class="container text-center">
         <p class="fs-1 bold">Quality plugins, at no cost.</span></p>
