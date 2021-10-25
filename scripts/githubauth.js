081d6ddd8c059ae55dca3cf5fae35a368ecaf158
Hilex23 <89878821+Hilex23@users.noreply.github.com>
2021-10-25 17:53:07 +0200

    Fix

---
+++ scripts/githubauth.js
@@ -0,0 +1,11 @@
+// Work in progress
+// This file is supposed to be the script file for the github authentication
+// Allows signing in and up at the website with github
+
+// Dashboard and settings will be accessable when logged in with Github
+// Profile will be consistent with Github on Varumux
+
+// Github consistency:
+// - avatar
+// - profile description
+// - profile name
