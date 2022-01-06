a462be4f341077c660e4d71fe867105ad794844a
joshua <zoniqhedgehog@gmail.com>
2022-01-06 13:51:47 +0100

    Change homepage to be user-friendly and not ugly

--- index.php
+++ index.php
@@ -3,52 +3,29 @@
 
 <?php include 'Templates/head.html' ;?>
 
+<header>
+    <img src="/assets/images/Varumox.png" height="100">
+    <header>
+        <div>
+            <div class="mask container">
+                <div class="d-flex justify-content-center align-items-center h-100">
+                    <div class="text-center">
+                        <h1 class="typewriter text-highlight"></h1>
+                        <h4 class="mb-3 text-center text-white">Varumox is a fully open-source and completely free minecraft plugin marketplace to get your quality plugins from.<br/>Varumox is a product of PsychOps Studios <?php echo " &copy; " . date("Y"); ?> developed by the PsychOps team.</h4>
+
+                        <a class="btn btn-primary" href="plugins.php">Plugins</a>
+                        <a class="btn btn-secondary">Sign in</a> <!-- CHECK LAST LINE FOR ELABORATION ON THIS -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</header>
+
 <body>
-<?php include 'Templates/header.html' ;?>
-
-<img src="assets/images/Varumox.png" alt="" style="background-color: var(--color-main)" width="100%">
-
-<div class="background" id="about">
-    <div class="container text-center" id="description">
-        <p class="bold fs-1 explicified">Quality plugins, at no cost.</span></p>
-        <p>Varumox strives to provide Minecraft plugin authors and users with an <a class="explicified bold" href="#UI">easy-to-use UI</a> offering <a class="explicified bold" href="#Free">completely free plugins</a>. Additionally, Varumox offers various <a class="explicified bold" href="#API">authentication-free API routes</a> to get plugin information or files from. Varumox is <a class="explicified bold" href="#Open-source">completely open source</a> <a href="https://github.com/PsychOps/Varumox" target="_blank">on GitHub</a>.
-    </div>
-
-    <div class="container topspace-50 text-start" id="UI">
-    <!-- <i class="fas fa-balance-scale fa-fw me-3"></i><span>Legal</span> -->
-        <a href=#UI>
-        <i class="fas fa-link explicified"></i>
-        <span class="bold fs-2 explicified">easy-to-use UI</span>
-        </a>
-        <p class="textwidth text-start">Varumox has a convenient and simple design, but pulls the most out of it by offering users a wide variety of features related to both searching for plugins, as well as uploading plugins and updating plugins on our website.</p>
-    </div>
-
-    <div class="container topspace-50 text-end" id="Free">
-            <a href=#Free>
-                <span class="bold fs-2 explicified">Completely free plugins</span>
-                <i class="fas fa-link explicified"></i>
-            </a>
-            <p class="textwidth text-end" style="margin-left: 50%">Varumox is free forever. This includes not only the project itself, but also the plugins listed on it. We do not offer any way to redirect to paid plugin websites or to make them paid on Varumox.</p>
-    </div>
-
-    <div class="container topspace-50 text-start" id="API">
-        <a href=#API>
-            <span class="bold fs-2 explicified">Authorization-free API</span>
-            <i class="fas fa-link explicified"></i>
-        </a>
-        <p class="textwidth text-start">Varumox offers a freely usable API to fetch data from in JSON format. Offering a variety of different and detailed, endpoints, it is designed to make getting data as easy as possible without the necessity of using an API token. The API can grab data about both authors and plugins, including plugin files.</p>
-    </div>
-
-    <div class="container topspace-50 text-end" id="Open-source">
-        <a href=#Open-source>
-            <i class="fas fa-link explicified"></i>
-            <span class="bold fs-2 explicified">Completely open source</span>
-        </a>
-        <p class=" textwidth text-end" style="margin-left: 50%">Varumox is completely open source. This means that its code is publicly accessible on GitHub but it's tied to the <a href="https://psychops.tk/legal.php" target=_blank>GPL-3.0 (GNU General Public License v3.0) license.</a> Feel free to participate on the project if you want to change anything. We will give to all those who help proper credit on the <a href="https://psychops.tk/credit.php">PsychOps website</a></p>
-    </div>
-<div class="background">
-</div>
-
 </body>
 
 </html>
+
+<!--
+The Sign In button should check whether the user that visits the website has already logged in before. If the user did log in, it should instead be a button that leads to the profile page. -->
