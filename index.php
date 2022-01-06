3d9f78652da27fcd5e788dea84ab6e102403fc0a
Hilex23 <unknowname.199@gmail.com>
2022-01-06 21:08:14 +0100

    Added PsychOps

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
 
 <header>
     <img src="/assets/images/Varumox.png" height="100">
+    <img class="poimg" src="/assets/images/PsychOpsTransparent.png" height="100">
     <header>
         <div>
             <div class="mask container">
                 <div class="d-flex justify-content-center align-items-center h-100">
                     <div class="text-center">
                         <h1 class="typewriter text-highlight"></h1>
-                        <h4 class="mb-3 text-center text-white">Varumox is a fully open-source and completely free minecraft plugin marketplace to get your quality plugins from.<br/>Varumox is a product of PsychOps Studios <?php echo " &copy; " . date("Y"); ?> developed by the PsychOps team.</h4>
+                        <h4 class="mb-3 text-center text-white">Varumox is a fully open-source and completely free minecraft plugin marketplace to get your quality plugins from.<br/>Varumox is a product of <a href="https://psychops.tk" target="_blank">PsychOps Studios</a> <?php echo " &copy; " . date("Y"); ?> developed by the PsychOps team.</h4>
 
                         <a class="btn btn-primary" href="plugins.php">Plugins</a>
                         <a class="btn btn-secondary">Sign in</a> <!-- CHECK LAST LINE FOR ELABORATION ON THIS -->
